3726f44c4cacdd53ab0962b46d7fad877dc8a083
veronnah <yoo.veroon@gmail.com>
2021-08-27 01:03:09 +0300

    2 sections responsive

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector(".body");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    body.classList.toggle("locked");
+}
